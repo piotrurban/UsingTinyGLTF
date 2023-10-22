aceb3824bbdb92037acf8dec87f2c6811d45c607
Piotr <urban.piotr1@gmail.com>
2023-10-22 20:21:40 +0200

    scene graph example working

--- src/TinyGltfTools/content_drawing.h
+++ src/TinyGltfTools/content_drawing.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Content.h"
 
-void draw_model(tinygltf::Model& model);
+void draw_model(Content& model);
 void draw_node(Content& model, const tinygltf::Node& node);
 void draw_mesh(Content& content, tinygltf::Mesh& mesh);
--- src/UseSceneGraph/use_scene_graph_main.cpp
+++ src/UseSceneGraph/use_scene_graph_main.cpp
@@ -1,4 +1,72 @@
+#include <gl_includes.h>
+#include <content_drawing.h>
+#include <BallTracker.h>
+#include <utils.h>
+#include <TransformUtils.h>
+
+#include <filesystem>
+#include <iostream>
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
 int main()
 {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 
+	if (!glfwInit())
+	{
+		std::cout << "glfwInit failed!\n";
+		exit(1);
+	}
+
+	GLFWwindow* window;
+	constexpr int window_width{ 800 };
+	constexpr int window_height{ 600 };
+	window = glfwCreateWindow(window_width, window_height, "scene graph example", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "glewInit failed!\n";
+		exit(2);
+	}
+
+	BallTracker& ballTracker = BallTracker::getInstance();
+	ballTracker.init();
+	ballTracker.setWindowSize(window_width, window_height);
+	ballTracker.registerWithGLFW(window);
+
+	std::filesystem::path base_dir{ TOSTRING(PROJECT_INCLUDE_DIR) };
+	const std::filesystem::path model_path{ base_dir / ".." / "model/scene_graph_example" };
+	const std::filesystem::path model_gltf{ model_path / "scene_graph.gltf" };
+	const std::filesystem::path model_vert{ model_path / "../shader.vert" };
+	const std::filesystem::path model_frag{ model_path / "../shader.frag" };
+	Content scene_graph_content(model_gltf.string(), model_vert.string(), model_frag.string());
+	scene_graph_content.setup_mesh_data();
+	reshapeFunc(window, window_width, window_height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		ballTracker.setCamera();
+
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glEnable(GL_DEPTH_TEST);
+
+		draw_model(scene_graph_content);
+
+		glMatrixMode(GL_PROJECTION);
+		glPopMatrix();
+
+		glFlush();
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
 }
